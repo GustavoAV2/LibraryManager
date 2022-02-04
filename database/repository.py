07a00817984d02e1ae4004035eca80cba311c9a5
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2022-02-04 13:35:41 -0300

    Creating new functions to crud

--- database/repository.py
+++ database/repository.py
@@ -18,3 +18,24 @@ def delete(model: object):
     _session.delete(model)
     commit()
     return model
+
+
+def update(model: object):
+    command = f"INSERT INTO {model.__tablename__} ({str(model)})"
+    _session.execute(command)
+    commit()
+    return model
+
+
+def get(model: object):
+    command = f"SELECT * FROM {model.__tablename__} "
+    _session.execute(command)
+    items = _session.fetchall()
+    return items
+
+
+def get_by_column(model: object, name_column: str, value_column):
+    command = f"SELECT * FROM {model.__tablename__} WHERE {name_column} == {value_column}"
+    _session.execute(command)
+    item = _session.fetchone()
+    return item
