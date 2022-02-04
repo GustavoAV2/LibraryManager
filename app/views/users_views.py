b7528ef73d595c342fa07cbcbcb02bfd32bb2d8d
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2022-02-04 12:33:17 -0300

    Creating new view

--- app/views/users_views.py
+++ app/views/users_views.py
@@ -1,60 +1,85 @@
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import (jwt_required, get_jwt_identity)
+from flask import jsonify, request
 from typing import Dict, Tuple, Any, List
-
-from app.actions.users_actions import \
-    get_user_by_id, get_users, update_user, create_user, login, deleted_user
-
-from app.models.users import User
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
+from app.actions.users_actions import *
+from app.actions.books_actions import *
 
 
-app_users = Blueprint('app.users', __name__)
+class View:
+    def __init__(self):
+        self.run = True
+        self.user = None
+        self.token = None
+
+    def main(self):
+        print('------ BEM VINDO A MARVIN-TECA -------')
+        self.create_user_or_login()
+
+        if self.run:
+            self.user = get_jwt_identity()
+            self.library_manager()
+
+    def create_user_or_login(self):
+        while self.run:
+            print('1 - Criar usuário.\n2 - Login\n3 - Encerrar sessão')
+            try:
+                op = int(input('Opcao:'))
+
+                if op == 1:
+                    payload = self.collect_user_data()
+                    self.user: User = create_user(payload)
+                elif op == 2:
+                    payload = self.collect_user_data()
+                else:
+                    self.run = False
+                    break
+
+                self.token = login(payload)
+                if self.token:
+                    break
+                else:
+                    print('Não foi possível criar usuário! Insira uma opcao:')
+            except (ValueError, TypeError):
+                print('Valor incorreto! Insira uma opcao:')
+                continue
+
+    def library_manager(self):
+        while self.run:
+            print('1 - Cadastrar livro.\n2 - Atualizar livro\n3 - Deletar livro\n4 - Encerrar sessão')
+            try:
+                op = int(input('Opcao:'))
+
+                if op == 1:
+                    self.collect_data_book()
+
+                elif op == 2:
+                    self.collect_data_book()
+
+                elif op == 3:
+                    self.collect_data_book()
+
+                else:
+                    self.run = False
+                    break
+
+            except (ValueError, TypeError):
+                print('Valor incorreto! Insira uma opcao:')
+                continue
+
+    @staticmethod
+    def collect_data_user():
+        email = input('E-mail:')
+        password = input('Senha:')
+        return {'email': email, 'password': password}
+
+    @staticmethod
+    def collect_data_book():
+        name = input('Nome:')
+        _type = input('Tipo:')
+        quantity = input('Quantidade:')
+        return {'name': name, 'type': _type, 'quantity': quantity}
 
 
-@app_users.route('/users/login', methods=['POST'])
-def login_user() -> Tuple[Any, int]:
-    user_login = request.get_json()
-    return jsonify(login(user_login)), 201
-
-
-@app_users.route('/users', methods=['POST'])
-def post() -> Tuple[Any, int]:
-    payload = request.get_json()
-    user: User = create_user(payload)
-    return jsonify(user.serialize()), 201
-
-
-@app_users.route('/users/<user_id>', methods=['PUT'])
-@jwt_required
-def update(user_id) -> Tuple[Any, int]:
-    payload = request.get_json()
-    user = update_user(user_id, payload)
-    return jsonify(user.serialize()), 201
-
-
-@app_users.route('/users', methods=['GET'])
-@jwt_required
-def get() -> Tuple[List[Dict], int]:
-    return jsonify([user.serialize() for user in get_users()]), 200
-
-
-@app_users.route('/users/<id>', methods=['GET'])
-@jwt_required
-def get_by_id(user_id) -> Tuple[Any, int]:
-    user = get_user_by_id(user_id)
-    return jsonify(user.serialize()), 200
-
-
-@app_users.route('/users/me', methods=['GET'])
-@jwt_required
-def view_user() -> Tuple[Any, int]:
-    user_id: str = get_jwt_identity()
-    user: User = get_user_by_id(user_id)
-    return jsonify(user.serialize()), 200
-
-
-@app_users.route('/users/delete/<id_user>', methods=['DELETE'])
-@jwt_required
-def delete(id_user) -> Tuple[Any, int]:
-    user: User = deleted_user(id_user)
-    return jsonify(user.serialize()), 200
+if __name__ == '__main__':
+    view = View()
+    view.main()
